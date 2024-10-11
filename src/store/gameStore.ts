fabc019945bac5b1070cda2a99176a2b5a86d0a4
Mateusz <m.wesolowski.92@gmail.com>
2024-10-11 16:41:43 +0200

    Update store - set empty array

--- src/store/gameStore.ts
+++ src/store/gameStore.ts
@@ -2,10 +2,14 @@ import { create } from 'zustand';
 import { GameStore } from './gameStore.types';
 
 export const useGameStore = create<GameStore>((set) => ({
-    matchedCards: [],
-    setMatchedCards: (matchedCards) => set({ matchedCards }),
+    matchedCardsId: [],
+    setMatchedCardsId: (id) =>
+        set((state) => ({
+            matchedCardsId: Array.isArray(id)
+                ? id
+                : [...state.matchedCardsId, id],
+        })),
     turnsCount: 0,
-    // increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
     setTurnsCount: (turnsCount) => set({ turnsCount }),
     time: 0,
     setTime: (time) => set({ time }),
