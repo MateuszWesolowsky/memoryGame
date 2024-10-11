f4d936868a4fba8b3bd475e14b2c1f6a9edc2fe1
Mateusz <m.wesolowski.92@gmail.com>
2024-10-11 16:42:00 +0200

    Update types

--- src/store/gameStore.types.ts
+++ src/store/gameStore.types.ts
@@ -1,8 +1,8 @@
 import { CardType } from '../types/types';
 
 export type GameStore = {
-    matchedCards: CardType[];
-    setMatchedCards: (newCards: CardType[]) => void;
+    matchedCardsId: CardType[];
+    setMatchedCardsId: (id: CardType[]) => void;
     turnsCount: number;
     setTurnsCount: (newCount: number) => void;
     time: number;
