b526b8e3c8dc78418c24a2322dee2b1db63423bf
Mateusz <m.wesolowski.92@gmail.com>
2024-10-11 19:12:11 +0200

    Create separate file for data

---
+++ src/utils/data.ts
@@ -0,0 +1,54 @@
+export const data = [
+    {
+        src: '/img/html.png',
+        name: 'html_card',
+        id: 'htmlId1',
+    },
+    {
+        src: '/img/html.png',
+        name: 'html_card',
+        id: 'htmlId2',
+    },
+    { src: '/img/css.png', name: 'css_card', id: 'cssId1' },
+    { src: '/img/css.png', name: 'css_card', id: 'cssId2' },
+    {
+        src: '/img/vite.png',
+        name: 'vite_card',
+        id: 'viteId1',
+    },
+    {
+        src: '/img/vite.png',
+        name: 'vite_card',
+        id: 'viteId2',
+    },
+    {
+        src: '/img/node.png',
+        name: 'node_card',
+        id: 'nodeId1',
+    },
+    {
+        src: '/img/node.png',
+        name: 'node_card',
+        id: 'nodeId2',
+    },
+    {
+        src: '/img/react.png',
+        name: 'react_card',
+        id: 'reactId1',
+    },
+    {
+        src: '/img/react.png',
+        name: 'react_card',
+        id: 'reactId2',
+    },
+    {
+        src: '/img/sass.png',
+        name: 'sass_card',
+        id: 'sassId1',
+    },
+    {
+        src: '/img/sass.png',
+        name: 'sass_card',
+        id: 'sassId2',
+    },
+];
